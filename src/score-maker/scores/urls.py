5809dc51c7a5c3fa8be06cfc1b45f3bf3469904e
Nikita Pisarev <nikgooder@gmail.com>
2024-09-30 15:18:09 +0300

    style: change the name of the endpoint to scores

--- src/score-maker/scores/urls.py
+++ src/score-maker/scores/urls.py
@@ -4,5 +4,5 @@ from scores.views import get_scores_history, set_score
 
 urlpatterns = [
     path('set-score/', set_score),
-    path('scores-history/', get_scores_history),
+    path('scores/', get_scores_history),
 ]
