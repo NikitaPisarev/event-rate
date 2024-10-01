3bfe7088f713aafe90438338359aed2a43cb3823
Nikita Pisarev <nikgooder@gmail.com>
2024-10-01 23:46:21 +0300

    feat: add haproxy

--- src/score-maker/config/settings.py
+++ src/score-maker/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["score-maker", "localhost", "0.0.0.0", "line-provider"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
