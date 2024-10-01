44d0484769732df5aaf514309d5574cb88baf79f
Nikita Pisarev <nikgooder@gmail.com>
2024-10-01 14:42:43 +0300

    style: add docstrings to endpoints

--- src/score-maker/scores/monitoring.py
+++ src/score-maker/scores/monitoring.py
@@ -6,4 +6,13 @@ from adrf.decorators import api_view
 
 @api_view(['GET'])
 async def health_check(request: Request) -> Response:
+    """
+    Checks the health of the API.
+
+    Args:
+        request (Request): The incoming HTTP request.
+
+    Returns:
+        Response: A JSON response containing the status of the API, with a status code of 200.
+    """
     return Response({'status': 'OK'}, status=status.HTTP_200_OK)
